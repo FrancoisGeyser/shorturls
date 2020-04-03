44ed84c2a62609930b74c3a57d6829eb09708cf7
Francois Geyser <fegeyser@gmail.com>
2020-04-03 20:39:05 +0300

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var db = require('diskdb');
 db = db.connect('./data', ['urls']);
 const shortid = require('shortid');
 shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-');
-const baseUrl = 'your-url.com'
+const baseUrl = 'surls.host'
 app.set('subdomain offset',1)
 
 app.get('/api/surls/getall', (req, res) => {
@@ -16,11 +16,11 @@ app.get('/api/surls/getone', (req, res) => {
 }) 
 
 app.get('/api/surls/manual', (req, res) => {
-    const surl = 'http://'+req.query.shorturl
+    const surl = req.query.shorturl
     const url = req.query.url
     db.urls.save({ surl: surl, url: url })
     const target = db.urls.find({ surl: surl })
-    res.send(`${target[0].surl}.${baseUrl}`)
+    res.send(`https://${target[0].surl}.${baseUrl}`)
 })
 
 app.get('/api/surls/new', (req, res) => {
@@ -28,15 +28,16 @@ app.get('/api/surls/new', (req, res) => {
     const url = req.query.url
     db.urls.save({ surl: surl, url: url })
     const target = db.urls.find({ surl: surl })
-    res.send(`${target[0].surl}.${baseUrl}`)
+    res.send(`https://${target[0].surl}.${baseUrl}`)
 })
 
 app.get('/', (req, res) => {
     const Url =  db.urls.find({ surl: req.subdomains[0] })
+	if (Url.length == 0){res.send('You missed something')}
     const TargetUrl = 'http://' + Url[0].url
       res.redirect(TargetUrl);
 });
 
 
-const port = process.env.port || 3000;
+const port = process.env.port || 8000;
 app.listen(port);
